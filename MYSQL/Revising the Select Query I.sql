18389b7d813fceedada842ddd7b1a28e904177ca
biratdatta <birat.datta26@gmail.com>
2023-05-08 23:53:51 +0530

    first commit

---
+++ MYSQL/Revising the Select Query I.sql
@@ -0,0 +1,10 @@
+/*  SELECT * FROM CITY WHERE population > 100000 AND Countrycode ="USA";
+
+
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+  */
+
+
+
+SELECT * FROM CITY WHERE population > 100000 AND Countrycode ="USA";
